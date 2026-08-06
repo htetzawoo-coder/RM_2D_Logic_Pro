5af677d3ba2f082e75afad3f9581e16775790e7a
htetzawoo-coder <htetzawoohzo99@gmail.com>
2026-08-06 21:30:00 +0630

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='rm2d-v125-entry-limit-ui';
+const CACHE='rm2d-v126-special-logic-buttons';
 const ASSETS=['./','./index.html','./manifest.json','./icon.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
