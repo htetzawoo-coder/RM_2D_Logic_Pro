3c76c40266dfd3699321e6322f48ac3772b5f488
htetzawoo-coder <htetzawoohzo99@gmail.com>
2026-08-08 15:42:11 +0630

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='rm2d-v134-complete-2d-formulas';
+const CACHE='rm2d-v135-mobile-entry-fit';
 const ASSETS=['./','./index.html','./manifest.json','./vendor/bootstrap.min.css','./vendor/chart.umd.js','./favicon-32x32.png','./apple-touch-icon.png','./icon-144x144.png','./icon-192x192.png','./icon-512x512.png','./maskable-icon-192x192.png','./maskable-icon-512x512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
