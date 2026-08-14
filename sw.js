92ffd7d50b423e329696a965ad72943bb7b0f101
htetzawoo-coder <htetzawoohzo99@gmail.com>
2026-08-14 06:40:39 +0630

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='rm2d-v137-global-filter';
+const CACHE='rm2d-v138-guided-entry';
 const ASSETS=['./','./index.html','./manifest.json','./vendor/bootstrap.min.css','./vendor/chart.umd.js','./favicon-32x32.png','./apple-touch-icon.png','./icon-144x144.png','./icon-192x192.png','./icon-512x512.png','./maskable-icon-192x192.png','./maskable-icon-512x512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
